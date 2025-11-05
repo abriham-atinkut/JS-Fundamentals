927f5860620de4cfb3a43ac90f65f39e49739933
unknown <abrihamatinkut@gmail.com>
2025-11-05 22:25:02 +0300

    Test Arguments and prints a message

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+function checkout(argu) {
+    if(argu === null) {
+        console.log("No argument");
+    } else if (argu.length === 1) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
+        
+    }
+}
