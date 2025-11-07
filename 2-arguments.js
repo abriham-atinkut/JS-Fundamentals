2cf94989209a79c8734cd3b9ac93513268dc7c33
unknown <abrihamatinkut@gmail.com>
2025-11-08 00:11:27 +0300

    last updata

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,7 @@
 let arg = process.argv.length;
+let argv_2 = process.argv[2];
 const expected = 3;
-    if (arg == null) {
+    if (argv_2 == null) {
       console.log("No argument");
     } else if (arg === expected) {
       console.log("Argument found");
