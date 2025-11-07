b0be21dbd898bf0b9caecefafa821a828e8fa2c6
unknown <abrihamatinkut@gmail.com>
2025-11-07 23:40:51 +0300

    prints the addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,8 @@
+let num_1 = Number(process.argv[2]);
+let num_2 = Number(process.argv[3]);
+
+function sum(num_1, num_2) {
+    let add = num_1 + num_2;
+    console.log(add);
+}
+sum(num_1, num_2);
