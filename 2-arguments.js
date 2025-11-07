992cb26e4d028c4a4e94ddb335bc5c6a557f03aa
unknown <abrihamatinkut@gmail.com>
2025-11-08 00:07:02 +0300

    arguments updata

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-let arg = process.argv[2]
-
+let arg = process.argv.length;
+const expected = 3;
     if (arg == null) {
       console.log("No argument");
-    } else if (arg != null) {
+    } else if (arg === expected) {
       console.log("Argument found");
     } else {
       console.log("Arguments found");
